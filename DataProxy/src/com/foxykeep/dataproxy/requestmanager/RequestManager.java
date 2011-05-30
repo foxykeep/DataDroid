23b6bd54cd65cfe351ee9f0a1e9391ff303e5dd9
Foxykeep <foxykeep@gmail.com>
2011-05-30 10:36:32 +0200

    [Lib] Add of the missing licence in class RequestManager

--- DataProxy/src/com/foxykeep/dataproxy/requestmanager/RequestManager.java
+++ DataProxy/src/com/foxykeep/dataproxy/requestmanager/RequestManager.java
@@ -1,5 +1,12 @@
+/*
+ * 2011 Foxykeep (http://www.foxykeep.com)
+ *
+ * Licensed under the Beerware License :
+ * 
+ *   As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think
+ *   this stuff is worth it, you can buy me a beer in return
+ */
 package com.foxykeep.dataproxy.requestmanager;
-
 
 /**
  * {@link RequestManager} is the superclass of the classes that will be
