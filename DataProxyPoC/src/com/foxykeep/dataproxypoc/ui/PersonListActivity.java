01ff243c26f108572cf92e00f3cd212197a4a1e6
Foxykeep <foxykeep@gmail.com>
2011-05-31 10:12:42 +0200

    Integration PersonList WS in PersonListActivity
    Still needs to work on the Adapter as well as clear the warnings

--- DataProxyPoC/src/com/foxykeep/dataproxypoc/ui/PersonListActivity.java
+++ DataProxyPoC/src/com/foxykeep/dataproxypoc/ui/PersonListActivity.java
@@ -1,7 +1,187 @@
 package com.foxykeep.dataproxypoc.ui;
 
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.CursorAdapter;
 
-public class PersonListActivity extends ListActivity {
+import com.foxykeep.dataproxypoc.R;
+import com.foxykeep.dataproxypoc.data.provider.PoCContent.PersonDao;
+import com.foxykeep.dataproxypoc.data.requestmanager.PoCRequestManager;
+import com.foxykeep.dataproxypoc.data.requestmanager.PoCRequestManager.OnRequestFinishedListener;
+import com.foxykeep.dataproxypoc.data.service.PoCService;
+import com.foxykeep.dataproxypoc.data.worker.PersonListWorker;
+import com.foxykeep.dataproxypoc.util.NotifyingAsyncQueryHandler;
+import com.foxykeep.dataproxypoc.util.NotifyingAsyncQueryHandler.AsyncQueryListener;
 
+public class PersonListActivity extends ListActivity implements OnRequestFinishedListener, AsyncQueryListener {
+
+    private static final String SAVED_STATE_REQUEST_ID = "savedStateRequestId";
+    private static final String SAVED_STATE_ARE_PERSONS_LOADED = "savedStateArePersonsLoaded";
+
+    private static final int DIALOG_CONNEXION_ERROR = 1;
+
+    private PoCRequestManager mRequestManager;
+    private int mRequestId = -1;
+
+    private boolean mArePersonsLoaded = false;
+
+    private NotifyingAsyncQueryHandler mQueryHandler;
+
+    private LayoutInflater mInflater;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
+        bindViews();
+
+        if (savedInstanceState != null) {
+            mRequestId = savedInstanceState.getInt(SAVED_STATE_REQUEST_ID, -1);
+            mArePersonsLoaded = savedInstanceState.getBoolean(SAVED_STATE_ARE_PERSONS_LOADED, false);
+        }
+
+        mRequestManager = PoCRequestManager.getInstance(this);
+        mQueryHandler = new NotifyingAsyncQueryHandler(getContentResolver(), this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mRequestId != -1) {
+            if (mRequestManager.isRequestInProgress(mRequestId)) {
+                mRequestManager.addOnRequestFinishedListener(this);
+                setProgressBarIndeterminateVisibility(true);
+            } else {
+                mRequestId = -1;
+                // TODO gestion des infos en base pour voir si requete OK
+                if (!true) {
+                    showDialog(DIALOG_CONNEXION_ERROR);
+                } else {
+                    mArePersonsLoaded = true;
+
+                    mQueryHandler.startQuery(PersonDao.CONTENT_URI, PersonDao.CONTENT_NAME_PROJECTION,
+                            PersonDao.LAST_NAME_ORDER_BY);
+                }
+            }
+        } else if (!mArePersonsLoaded) {
+            callPersonListWS();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mRequestId != -1) {
+            mRequestManager.removeOnRequestFinishedListener(this);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(SAVED_STATE_REQUEST_ID, mRequestId);
+    }
+
+    private void bindViews() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(final int id) {
+        Builder b;
+        switch (id) {
+            case DIALOG_CONNEXION_ERROR:
+                b = new Builder(this);
+                b.setCancelable(false);
+                b.setNeutralButton(getString(android.R.string.ok), null);
+                b.setPositiveButton(getString(R.string.dialog_button_retry), new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int which) {
+                        callPersonListWS();
+                    }
+                });
+                b.setTitle(R.string.dialog_error_connexion_error_title);
+                b.setMessage(R.string.dialog_error_connexion_error_message);
+                return b.create();
+            default:
+                return super.onCreateDialog(id);
+        }
+    }
+
+    private void callPersonListWS() {
+        setProgressBarIndeterminateVisibility(true);
+        mRequestManager.addOnRequestFinishedListener(this);
+        mRequestId = mRequestManager.getPersonList(PersonListWorker.RETURN_FORMAT_XML);
+    }
+
+    @Override
+    public void onRequestFinished(final int requestId, final int resultCode, final Bundle payload) {
+        if (requestId == mRequestId) {
+            setProgressBarIndeterminateVisibility(false);
+            mRequestId = -1;
+            if (resultCode == PoCService.ERROR_CODE) {
+                if (payload != null) {
+                    final int errorType = payload.getInt(PoCRequestManager.RECEIVER_EXTRA_ERROR_TYPE, -1);
+                    if (errorType == PoCRequestManager.RECEIVER_EXTRA_VALUE_ERROR_TYPE_DATA) {
+                        // TODO gérer ce cas
+                        // mErrorDialogTitle =
+                        // getString(R.string.dialog_error_data_error_title);
+                        // mErrorDialogMessage =
+                        // getString(R.string.dialog_error_data_error_message);
+                        // showDialog(DIALOG_ERROR);
+                    } else {
+                        showDialog(DIALOG_CONNEXION_ERROR);
+                    }
+                } else {
+                    showDialog(DIALOG_CONNEXION_ERROR);
+                }
+            } else {
+                mArePersonsLoaded = true;
+
+                mQueryHandler.startQuery(PersonDao.CONTENT_URI, PersonDao.CONTENT_NAME_PROJECTION,
+                        PersonDao.LAST_NAME_ORDER_BY);
+            }
+        }
+    }
+
+    @Override
+    public void onQueryComplete(final int token, final Object cookie, final Cursor cursor) {
+        PersonListAdapter adapter = (PersonListAdapter) getListAdapter();
+        if (adapter == null) {
+            adapter = new PersonListAdapter(this, cursor);
+            setListAdapter(adapter);
+        } else {
+            adapter.changeCursor(cursor);
+        }
+    }
+
+    class PersonListAdapter extends CursorAdapter {
+
+        public PersonListAdapter(final Context context, final Cursor c) {
+            super(context, c);
+        }
+
+        @Override
+        public void bindView(final View view, final Context context, final Cursor cursor) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public View newView(final Context context, final Cursor cursor, final ViewGroup parent) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }
 }
