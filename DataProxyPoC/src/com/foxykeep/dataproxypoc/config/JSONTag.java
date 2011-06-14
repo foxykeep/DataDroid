6ce253cfe47a00b95ee063b1ea5a2a52868b5781
Foxykeep <foxykeep@gmail.com>
2011-06-14 07:59:46 +0200

    [PoC] Rename some missing fields and the sample WS files

--- DataProxyPoC/src/com/foxykeep/dataproxypoc/config/JSONTag.java
+++ DataProxyPoC/src/com/foxykeep/dataproxypoc/config/JSONTag.java
@@ -3,15 +3,15 @@ package com.foxykeep.dataproxypoc.config;
 public class JSONTag {
 
     // PersonList WS tags
-    public static final String ELEM_PERSONS = "persons";
-    public static final String ELEM_PERSON = "person";
-    public static final String ELEM_PERSON_FIRST_NAME = "firstName";
-    public static final String ELEM_PERSON_LAST_NAME = "lastName";
-    public static final String ELEM_PERSON_EMAIL = "email";
-    public static final String ELEM_PERSON_CITY = "city";
-    public static final String ELEM_PERSON_POSTAL_CODE = "postalCode";
-    public static final String ELEM_PERSON_AGE = "age";
-    public static final String ELEM_PERSON_IS_WORKING = "isWorking";
+    public static final String PERSON_LIST_ELEM_PERSONS = "persons";
+    public static final String PERSON_LIST_ELEM_PERSON = "person";
+    public static final String PERSON_LIST_ELEM_PERSON_FIRST_NAME = "firstName";
+    public static final String PERSON_LIST_ELEM_PERSON_LAST_NAME = "lastName";
+    public static final String PERSON_LIST_ELEM_PERSON_EMAIL = "email";
+    public static final String PERSON_LIST_ELEM_PERSON_CITY = "city";
+    public static final String PERSON_LIST_ELEM_PERSON_POSTAL_CODE = "postalCode";
+    public static final String PERSON_LIST_ELEM_PERSON_AGE = "age";
+    public static final String PERSON_LIST_ELEM_PERSON_IS_WORKING = "isWorking";
 
     // CityList WS tags
     public static final String CITY_LIST_ELEM_CITIES = "cities";
--- DataProxyPoC/src/com/foxykeep/dataproxypoc/data/factory/PersonListJsonFactory.java
+++ DataProxyPoC/src/com/foxykeep/dataproxypoc/data/factory/PersonListJsonFactory.java
@@ -15,20 +15,20 @@ public class PersonListJsonFactory {
         final ArrayList<Person> personList = new ArrayList<Person>();
 
         final JSONObject parser = new JSONObject(wsResponse);
-        final JSONObject jsonRoot = parser.getJSONObject(JSONTag.ELEM_PERSONS);
-        final JSONArray jsonPersonArray = jsonRoot.getJSONArray(JSONTag.ELEM_PERSON);
+        final JSONObject jsonRoot = parser.getJSONObject(JSONTag.PERSON_LIST_ELEM_PERSONS);
+        final JSONArray jsonPersonArray = jsonRoot.getJSONArray(JSONTag.PERSON_LIST_ELEM_PERSON);
         final int size = jsonPersonArray.length();
         for (int i = 0; i < size; i++) {
             final JSONObject jsonPerson = jsonPersonArray.getJSONObject(i);
             final Person person = new Person();
 
-            person.firstName = jsonPerson.getString(JSONTag.ELEM_PERSON_FIRST_NAME);
-            person.lastName = jsonPerson.getString(JSONTag.ELEM_PERSON_LAST_NAME);
-            person.email = jsonPerson.getString(JSONTag.ELEM_PERSON_EMAIL);
-            person.city = jsonPerson.getString(JSONTag.ELEM_PERSON_CITY);
-            person.postalCode = jsonPerson.getInt(JSONTag.ELEM_PERSON_POSTAL_CODE);
-            person.age = jsonPerson.getInt(JSONTag.ELEM_PERSON_AGE);
-            person.isWorking = jsonPerson.getInt(JSONTag.ELEM_PERSON_IS_WORKING) == 1;
+            person.firstName = jsonPerson.getString(JSONTag.PERSON_LIST_ELEM_PERSON_FIRST_NAME);
+            person.lastName = jsonPerson.getString(JSONTag.PERSON_LIST_ELEM_PERSON_LAST_NAME);
+            person.email = jsonPerson.getString(JSONTag.PERSON_LIST_ELEM_PERSON_EMAIL);
+            person.city = jsonPerson.getString(JSONTag.PERSON_LIST_ELEM_PERSON_CITY);
+            person.postalCode = jsonPerson.getInt(JSONTag.PERSON_LIST_ELEM_PERSON_POSTAL_CODE);
+            person.age = jsonPerson.getInt(JSONTag.PERSON_LIST_ELEM_PERSON_AGE);
+            person.isWorking = jsonPerson.getInt(JSONTag.PERSON_LIST_ELEM_PERSON_IS_WORKING) == 1;
 
             personList.add(person);
         }
