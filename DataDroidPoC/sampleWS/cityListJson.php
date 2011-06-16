3c3af23ceafd4ec9909888622a5e1ae044788070
Foxykeep <foxykeep@gmail.com>
2011-06-16 18:14:58 +0200

    [PoC] Update the WS samples

--- DataDroidPoC/sampleWS/cityListJson.php
+++ DataDroidPoC/sampleWS/cityListJson.php
@@ -1,19 +1,20 @@
-{
-	cities:{
-		city:[
-			{
+<?php 
+header ("content-type: text/json");
+echo "{
+    cities:{
+        city:[
+            {
 				name:'Saint Herblain',
 				postalCode:44800,
 				countyNumber:44,
 				countyName:'Loire Atlantique'
-			},
-			{
+            },
+            {
 				name:'Cesson Sévigné',
 				postalCode:35510,
 				countyNumber:35,
 				countyName:'Ille et Vilaine'
-			}
-		]
-	}
-}
-			
+            }
+        ]
+    }
+}";?>
