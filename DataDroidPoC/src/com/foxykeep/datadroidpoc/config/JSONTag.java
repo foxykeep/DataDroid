fc45e1879696398c8e39b257a434ee4b815111b5
Foxykeep <foxykeep@gmail.com>
2011-06-21 11:51:56 +0200

    Add the missing Beerware licence

--- DataDroidPoC/src/com/foxykeep/datadroidpoc/config/JSONTag.java
+++ DataDroidPoC/src/com/foxykeep/datadroidpoc/config/JSONTag.java
@@ -1,3 +1,11 @@
+/*
+ * 2011 Foxykeep (http://www.foxykeep.com)
+ *
+ * Licensed under the Beerware License :
+ * 
+ *   As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think
+ *   this stuff is worth it, you can buy me a beer in return
+ */
 package com.foxykeep.datadroidpoc.config;
 
 public class JSONTag {
